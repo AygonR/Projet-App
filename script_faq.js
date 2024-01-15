940fadd574092151d9444ccd5fb9a920042857c8
AygonR <103449788+AygonR@users.noreply.github.com>
2024-01-15 04:02:41 +0100

    Update script_faq.js

--- script_faq.js
+++ script_faq.js
@@ -11,6 +11,7 @@ allCross.forEach(element => {
         const currentChoice = this.parentNode.parentNode.childNodes[3];
 
         // console.log(this.src);
+
         if(this.src.includes('croix')){
             this.src = 'moins.png';
             gsap.to(currentChoice, {duration: 0.2, height: height + 40, opacity: 1, padding: '20px 15px'})
@@ -18,31 +19,26 @@ allCross.forEach(element => {
             this.src = 'croix.png';
             gsap.to(currentChoice, {duration: 0.2, height: 0, opacity: 0, padding: '0px 15px'})
         }
-        
+
     })
 
 })
 
 
-document.getElementById('monFormulaire').addEventListener('submit', function(event) {
+document.getElementById('formu').addEventListener('submit', function(event) {
     event.preventDefault(); //On empêche l'envoi
-
     //On récupère le contenu du champ
-    var message = document.getElementById('monTxt').value;
-
+    var message = document.getElementById('query').value;
     //On vérifie si le champ est vide
     if (!message.trim()) {
         return; //On arrête l'exécution de la fonction si le champ est vide
     }
-
     //On crée une nouvelle div pour la bannière
     var banner = document.createElement('div');
     banner.className = 'banner'
-
     //On applique le contenu de la bannière
     var message = document.getElementById('message').textContent;
     banner.textContent = message;
-
     banner.style.backgroundColor = 'rgba(41, 122, 0, 0.5)';
     banner.style.color = "white";
     banner.style.position = 'fixed';
@@ -51,10 +47,8 @@ document.getElementById('monFormulaire').addEventListener('submit', function(eve
     banner.style.padding = '10px';
     banner.style.textAlign = 'center';
     banner.style.fontSize = '40px';
-
     // On attache la bannière au body
     document.body.appendChild(banner);
-
     // On retire la bannière après 5 secondes
     setTimeout(function() {
         banner.style.animation = 'slideOut 1s forwards';
@@ -62,12 +56,43 @@ document.getElementById('monFormulaire').addEventListener('submit', function(eve
             document.body.removeChild(banner);
     }, 1000);
     }, 6000);;
-    this.reset();
+    // Retarder le reset de 3 secondes
+    setTimeout(function() {
+        this.reset();
+    }.bind(this), 200); // 'this' est lié à la fonction pour conserver le contexte de l'élément du formulaire
+    
 
 
-document.getElementById('monTxt').addEventListener('focus',function(){this.placeholder = '';})
-document.getElementById('monTxt').addEventListener('blur',function(){
+document.getElementById('query').addEventListener('focus',function(){this.placeholder = '';})
+document.getElementById('query').addEventListener('blur',function(){
     if(this.value ===''){
         this.placeholder = 'Saisissez votre question';
     }
 })}) 
+
+
+document.getElementById("myButton1").addEventListener("click", function() {
+    window.location.href = "https://fr.linkedin.com/school/institut-sup%C3%A9rieur-d'electronique-de-paris/"; // Remplacez "nouvelle_page.html" par le chemin de la nouvelle page
+});
+
+document.getElementById("myButton2").addEventListener("click", function() {
+    window.location.href = "https://www.instagram.com/isepparis/"; // Remplacez "nouvelle_page.html" par le chemin de la nouvelle page
+});
+
+document.getElementById("myButton3").addEventListener("click", function() {
+    window.location.href = "https://fr-fr.facebook.com/ISEP.Paris/"; // Remplacez "nouvelle_page.html" par le chemin de la nouvelle page
+});
+
+    $(document).ready(function(){
+        $("#formu").on("submit", function(event){
+            event.preventDefault(); // Empêche le rechargement de la page
+            var formData = new FormData(this);
+            $.ajax({
+                url: "q_posee.php", // L'URL de votre script PHP
+                type: "post",
+                data: formData, 
+                processData: false,  // Indique à jQuery de ne pas traiter les données
+                contentType: false   // Indique à jQuery de ne pas définir le type de contenu
+            });
+        });
+    });
